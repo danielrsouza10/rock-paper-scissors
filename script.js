f68730b4657a95e1b812a1707289e50cbb069736
Daniel Souza <danielrsouza10@gmail.com>
2023-04-23 22:31:01 -0300

    finally working on the browser (instead of the console). Still need to end the game after 5 rounds and add some style 'cause still pretty basic

--- script.js
+++ script.js
@@ -1,85 +1,77 @@
-const buttons = document.querySelectorAll('input')
-let playerScore = 0;
-let computerScore = 0;
+//Code for the HTML
+const score = document.querySelector('#score');
+const roundWinner = document.createElement('p');
+const result = document.createElement('p');
+const totalGames = document.createElement('p');
+const finalResult = document.createElement('h1');
+
+score.appendChild(roundWinner);
+score.appendChild(result);
+score.appendChild(totalGames);
+score.appendChild(finalResult);
+
+//Code for the game
+const choices = ["ROCK", "PAPER", "SCISSORS"];
+let playerPoints = 0;
+let computerPoints = 0;
 let gamesPlayed = 0;
+let playerChoice = 0;
 
-buttons.forEach(button =>{
-    button.addEventListener('click', function(){
-        playRound(button.value)
+const rockButton = document.querySelector('#rock-button');
+const paperButton = document.querySelector('#paper-button');
+const scissorsButton = document.querySelector('#scissors-button');
+const buttons = document.querySelectorAll('button');
+
+//for each button, the function(e) will store the value of innerHTML for the button that was pressed and store in the variable playerChoice
+buttons.forEach((button) =>
+    button.addEventListener('click', function (e) {
+        playerChoice = e.target.innerHTML;
+        
     })
-})
+)
 
-function computerPlay() {
-    const game = ['rock', 'paper', 'scissors'];
-    const computerSelection = Math.floor(Math.random() * game.length);
-    return game[computerSelection];
+rockButton.addEventListener('click', playRound);
+paperButton.addEventListener('click', playRound);
+scissorsButton.addEventListener('click', playRound);
+
+
+
+function computerSelection() {
+    //Math.floor round the element to an integer and Math.random select randomly from the array length
+    let chooseRandom = Math.floor(Math.random() * choices.length);
+    let computerChoice = choices[chooseRandom];
+    return computerChoice;
 }
 
-function playRound(playerSelection){
-    
-    let computerSelection = computerPlay();
+function playRound() {
+    //execute the function computerSelection and store in the variable
+    let computerChoice = computerSelection();
+
+    if (playerChoice === computerChoice) {
+        roundWinner.textContent = "Thats a DRAW!";
+        result.textContent = "SCORE => You " + playerPoints + " X " + computerPoints + " Machine.";
+
+    } else {
+        if (playerChoice === "PAPER" && computerChoice === "ROCK") {
+            playerPoints++;
+            roundWinner.textContent = "You win the round! PAPER wins over ROCK.";
+        } else if (playerChoice === "ROCK" && computerChoice === "SCISSORS") {
+            playerPoints++;
+            roundWinner.textContent = "You win the round! ROCK wins over SCISSORS.";
+        } else if (playerChoice === "SCISSORS" && computerChoice === "PAPER") {
+            playerPoints++;
+            roundWinner.textContent = "You win the round! SCISSORS wins over PAPER.";
+        } else {
+            roundWinner.textContent = "You lose the round! " + computerChoice + " wins over " + playerChoice + ".";
+            computerPoints++;
+        }
+        result.textContent = "SCORE => You " + playerPoints + " X " + computerPoints + " Machine.";
+
+
+    }
     gamesPlayed++;
-    
-
-    console.log (playerSelection);
-    console.log (computerSelection);
-
-    if (playerSelection === computerSelection) {
-        console.log('It is a tie!');
-        
-    }
-    else if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-        playerScore++;
-        console.log('You win the round! You: ' + playerScore + ' Machine: ' + computerScore);
-      
-    }
-    else {
-        computerScore++;
-        console.log('The machine wins the round! You: ' + playerScore + ' Machine: ' + computerScore);
-     
-
-    }
-    if(gamesPlayed > 5){
-        console.log('END OF GAME! The Result: You: ' + playerScore + ' ' + computerScore + ' Machine');
-        disableButtons();
-    }
-    // document.getElementById('result').innerHTML = result
-    // return
+    totalGames.textContent = " Games Played: " + gamesPlayed;
 }
-
-function disableButtons(){
-    buttons.forEach(elem => {
-        elem.disable = true;
-    })
-}
-
-// let playerScore = playRound;
-// console.log(playerScore);
-
-
-
-// function gameWinner(score) {
-//     const result = score;
-//     const playerScore = 0;
-//     const computerScore = 0;
-
-//     if (result === 'youWin') {
-//         playerScore++;
-//         console.log(playerScore + " " + computerScore);
-        
-//     }
-//     else if (result === 'machine') {
-//         computerScore++;
-//         console.log(playerScore + " " + computerScore);
-//     }
-//     else {
-//         console.log('It is a tie!');
-        
-
-//     }
-// }
-
-
 
 
 
